b9980454e09babc6a7cef46ddeba0fbacf44d49c
Maksonpipison <onishchuk2015op@gmail.com>
2023-11-15 13:20:22 +0300

    Добавьте файлы проекта.

---
+++ lab5/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+public class Sphere
+{
+    private double radius;
+
+    public double Radius
+    {
+        get { return radius; }
+        set
+        {
+            if (value >= 0)
+                radius = value;
+        }
+    }
+
+    public double CalculateVolume()
+    {
+        return (4.0 / 3.0) * Math.PI * Math.Pow(radius, 3);
+    }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        // Створення об'єкта класу Sphere
+        Sphere sphere = new Sphere();
+
+        // Введення та перевірка значення радіусу кулі
+        double radius;
+        Console.Write("Введіть радіус кулі: ");
+
+        while (!double.TryParse(Console.ReadLine(), out radius) || radius < 0)
+        {
+            Console.WriteLine("Введено некоректне значення. Введіть будь ласка додатнє число.");
+            Console.Write("Введіть радіус кулі: ");
+        }
+
+        // Встановлення значення радіусу кулі
+        sphere.Radius = radius;
+
+        // Обчислення та виведення об'єму кулі
+        Console.WriteLine($"Об'єм кулі з радіусом {sphere.Radius} дорівнює {sphere.CalculateVolume():F2}");
+    }
+}
